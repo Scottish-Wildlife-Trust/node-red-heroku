9f5b8e1d79b50f2fb227e61d166ea8383cc1524f
ruchirlives <39463877+ruchirlives@users.noreply.github.com>
2021-01-31 17:24:19 +0000

    Update settings.js
    Added request-promise

--- settings.js
+++ settings.js
@@ -43,7 +43,9 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: {
+            request:require('request-promise')
+        },
 
     storageModule: require("./pgstorage"),
 
